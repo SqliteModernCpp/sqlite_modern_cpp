458c78b21b29076419f72d3c9503527b8681a909
Adrian Kummerlaender <knairda@t-online.de>
2015-04-04 22:05:17 +0200

    Expanded conditionals and loops
    Single line `if` conditionals and `while` loops may be more compact but ease bug introduction in the future as well as being unnecessarily inconsistent.

--- sqlite_modern_cpp.h
+++ sqlite_modern_cpp.h
@@ -32,11 +32,7 @@ namespace sqlite {
 	class database;
 	class database_binder;
 
-	template<int N>
-	class binder {
-		template<typename F>
-		static void run(database_binder& dbb, F l);
-	};
+	template<int> class binder;
 
 	class database_binder {
 	private:
@@ -47,34 +43,46 @@ namespace sqlite {
 
 		void _extract(std::function<void(void)> call_back) {
 			int hresult;
-			while ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW)
+
+			while ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW) {
 				call_back();
+			}
 
-			if (hresult != SQLITE_DONE)
+			if (hresult != SQLITE_DONE) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
 
-			if (sqlite3_finalize(_stmt) != SQLITE_OK)
+			if (sqlite3_finalize(_stmt) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			_stmt = nullptr;
 		}
 		void _extract_single_value(std::function<void(void)> call_back) {
 			int hresult;
-			if ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW)
+
+			if ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW) {
 				call_back();
+			}
 
-			if ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW)
+			if ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW) {
 				throw std::runtime_error("not every row extracted");
+			}
 
-			if (hresult != SQLITE_DONE)
+			if (hresult != SQLITE_DONE) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
 
-			if (sqlite3_finalize(_stmt) != SQLITE_OK)
+			if (sqlite3_finalize(_stmt) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			_stmt = nullptr;
 		}
 		void _prepare() {
-			if (sqlite3_prepare16_v2(_db, _sql.data(), -1, &_stmt, nullptr) != SQLITE_OK)
+			if (sqlite3_prepare16_v2(_db, _sql.data(), -1, &_stmt, nullptr) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
 		}
 	protected:
 		database_binder(sqlite3 * db, std::u16string const & sql) :
@@ -93,87 +101,114 @@ namespace sqlite {
 			/* Will be executed if no >>op is found */
 			if (_stmt) {
 				int hresult;
-				while ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW)
-					;
+				while ((hresult = sqlite3_step(_stmt)) == SQLITE_ROW) { }
 
 				if (hresult != SQLITE_DONE) {
 					throw std::runtime_error(sqlite3_errmsg(_db));
 				}
 
-				if (sqlite3_finalize(_stmt) != SQLITE_OK)
+				if (sqlite3_finalize(_stmt) != SQLITE_OK) {
 					throw std::runtime_error(sqlite3_errmsg(_db));
+				}
+
 				_stmt = nullptr;
 			}
 		}
 #pragma region operator <<
 		database_binder& operator <<(double val) {
-			if (sqlite3_bind_double(_stmt, _inx, val) != SQLITE_OK)
+			if (sqlite3_bind_double(_stmt, _inx, val) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			++_inx;
 			return *this;
 		}
 		database_binder& operator <<(float val) {
-			if (sqlite3_bind_double(_stmt, _inx, double(val)) != SQLITE_OK)
+			if (sqlite3_bind_double(_stmt, _inx, double(val)) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			++_inx;
 			return *this;
 		}
 		database_binder& operator <<(int val) {
-			if (sqlite3_bind_int(_stmt, _inx, val) != SQLITE_OK)
+			if (sqlite3_bind_int(_stmt, _inx, val) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			++_inx;
 			return *this;
 		}
 		database_binder& operator <<(sqlite_int64 val) {
-			if (sqlite3_bind_int64(_stmt, _inx, val) != SQLITE_OK)
+			if (sqlite3_bind_int64(_stmt, _inx, val) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			++_inx;
 			return *this;
 		}
 		database_binder& operator <<(std::string const& txt) {
-			if (sqlite3_bind_text(_stmt, _inx, txt.data(), -1, SQLITE_TRANSIENT) != SQLITE_OK)
+			if (sqlite3_bind_text(_stmt, _inx, txt.data(), -1, SQLITE_TRANSIENT) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			++_inx;
 			return *this;
 		}
 		database_binder& operator <<(std::u16string const& txt) {
-			if (sqlite3_bind_text16(_stmt, _inx, txt.data(), -1, SQLITE_TRANSIENT) != SQLITE_OK)
+			if (sqlite3_bind_text16(_stmt, _inx, txt.data(), -1, SQLITE_TRANSIENT) != SQLITE_OK) {
 				throw std::runtime_error(sqlite3_errmsg(_db));
+			}
+
 			++_inx;
 			return *this;
 		}
 #pragma endregion
 
-#pragma region get_col_from_db			
+#pragma region get_col_from_db
 		void get_col_from_db(int inx, int& i) {
-			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) i = 0;
-			else i = sqlite3_column_int(_stmt, inx);
+			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) {
+				i = 0;
+			} else {
+				i = sqlite3_column_int(_stmt, inx);
+			}
 		}
 		void get_col_from_db(int inx, sqlite3_int64& i) {
-			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) i = 0;
-			else i = sqlite3_column_int64(_stmt, inx);
+			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) {
+				i = 0;
+			} else {
+				i = sqlite3_column_int64(_stmt, inx);
+			}
 		}
 		void get_col_from_db(int inx, std::string& s) {
-			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) s = std::string();
-			else {
+			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) {
+				s = std::string();
+			} else {
 				sqlite3_column_bytes(_stmt, inx);
 				s = std::string((char*)sqlite3_column_text(_stmt, inx));
 			}
 		}
 		void get_col_from_db(int inx, std::u16string& w) {
-			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) w = std::u16string();
-			else {
+			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) {
+				w = std::u16string();
+			} else {
 				sqlite3_column_bytes16(_stmt, inx);
 				w = std::u16string((char16_t *)sqlite3_column_text16(_stmt, inx));
 			}
 		}
 		void get_col_from_db(int inx, double& d) {
-			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) d = 0;
-			else d = sqlite3_column_double(_stmt, inx);
+			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) {
+				d = 0;
+			} else {
+				d = sqlite3_column_double(_stmt, inx);
+			}
 		}
 		void get_col_from_db(int inx, float& f) {
-			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) f = 0;
-			else f = float(sqlite3_column_double(_stmt, inx));
+			if (sqlite3_column_type(_stmt, inx) == SQLITE_NULL) {
+				f = 0;
+			} else {
+				f = float(sqlite3_column_double(_stmt, inx));
+			}
 		}
 #pragma endregion
 
