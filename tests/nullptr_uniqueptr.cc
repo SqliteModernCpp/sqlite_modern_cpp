e30408b646de81e6bb4d42786bd9e824ccd65ea7
amin roosta <amin.roosta@outlook.com>
2016-05-06 19:50:19 +0430

    reformat the test

--- tests/nullptr_uniqueptr.cc
+++ tests/nullptr_uniqueptr.cc
@@ -7,14 +7,14 @@ using namespace sqlite;
 
 int main() {
 
-	try {
-		database db(":memory:");
-		db << "CREATE TABLE tbl (id integer,age integer, name string, img blob);";
-		db << "INSERT INTO tbl VALUES (?, ?, ?, ?);" << 1 << 24 << "bob" << vector<int> { 1, 2 , 3};
+  try {
+    database db(":memory:");
+    db << "CREATE TABLE tbl (id integer,age integer, name string, img blob);";
+    db << "INSERT INTO tbl VALUES (?, ?, ?, ?);" << 1 << 24 << "bob" << vector<int> { 1, 2 , 3};
     unique_ptr<string> ptr_null;
-		db << "INSERT INTO tbl VALUES (?, ?, ?, ?);" << 2 << nullptr << ptr_null << nullptr;
+    db << "INSERT INTO tbl VALUES (?, ?, ?, ?);" << 2 << nullptr << ptr_null << nullptr;
 
-		db << "select age,name,img from tbl where id = 1" >> [](unique_ptr<int> age_p, unique_ptr<string> name_p, unique_ptr<vector<int>> img_p) {
+    db << "select age,name,img from tbl where id = 1" >> [](unique_ptr<int> age_p, unique_ptr<string> name_p, unique_ptr<vector<int>> img_p) {
       if(age_p == nullptr || name_p == nullptr || img_p == nullptr) {
         cerr << "ERROR: values should not be null" << std::endl;
         exit(EXIT_FAILURE);
@@ -22,26 +22,26 @@ int main() {
 
       cout << "age:" << *age_p << " name:" << *name_p << " img:";
       for(auto i : *img_p) cout << i << ","; cout << endl;
-		};
+    };
 
-		db << "select age,name,img from tbl where id = 2" >> [](unique_ptr<int> age_p, unique_ptr<string> name_p, unique_ptr<vector<int>> img_p) {
+    db << "select age,name,img from tbl where id = 2" >> [](unique_ptr<int> age_p, unique_ptr<string> name_p, unique_ptr<vector<int>> img_p) {
       if(age_p != nullptr || name_p != nullptr || img_p != nullptr) {
         cerr << "ERROR: values should be nullptr" << std::endl;
         exit(EXIT_FAILURE);
       }
 
       cout << "OK all three values are nullptr" << endl;
-		};
+    };
 
-	} catch(sqlite_exception e) {
-		cout << "Sqlite error " << e.what() << endl;
-		exit(EXIT_FAILURE);
-	} catch(...) {
-		cout << "Unknown error\n";
-		exit(EXIT_FAILURE);
-	}
+  } catch(sqlite_exception e) {
+    cout << "Sqlite error " << e.what() << endl;
+    exit(EXIT_FAILURE);
+  } catch(...) {
+    cout << "Unknown error\n";
+    exit(EXIT_FAILURE);
+  }
 
-	cout << "OK\n";
-	exit(EXIT_SUCCESS);
+  cout << "OK\n";
+  exit(EXIT_SUCCESS);
   return 0;
 }
