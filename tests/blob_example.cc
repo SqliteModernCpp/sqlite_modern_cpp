1f136e207f63d1912fbaa7eae58e3699b15bdff2
amin roosta <amin.roosta@outlook.com>
2017-05-24 03:46:39 +0430

    blob_t implementation

--- tests/blob_example.cc
+++ tests/blob_example.cc
@@ -13,44 +13,37 @@ int main()
 		database db(":memory:");
 
 		db << "CREATE TABLE person (name TEXT, numbers BLOB);";
-		db << "INSERT INTO person VALUES (?, ?)" << "bob" << vector<int> { 1, 2, 3, 4};
-		db << "INSERT INTO person VALUES (?, ?)" << "jack" << vector<char> { '1', '2', '3', '4'};
-		db << "INSERT INTO person VALUES (?, ?)" << "sara" << vector<double> { 1.0, 2.0, 3.0, 4.0};
+		db << "INSERT INTO person VALUES (?, ?)" << "bob" << blob(vector<int> { 1, 2, 3});
+		db << "INSERT INTO person VALUES (?, ?)" << "jack" << blob(vector<char> { '1', '2', '3'});
+		db << "INSERT INTO person VALUES (?, ?)" << "sara" << blob(vector<double> { 1.0, 2.0, 3.0});
 
+		// Extract to lvalue blob
 		vector<int> numbers_bob;
-		db << "SELECT numbers from person where name = ?;" << "bob" >> numbers_bob;
+		db << "SELECT numbers from person where name = ?;" << "bob" >> blob(numbers_bob);
 
-		if(numbers_bob.size() != 4 || numbers_bob[0] != 1
-        || numbers_bob[1] != 2 || numbers_bob[2] != 3 || numbers_bob[3] != 4 ) {
+
+		if(numbers_bob.size() != 3 || numbers_bob[0] != 1 || numbers_bob[1] != 2 || numbers_bob[2] != 3) {
 			cout << "Bad result on line " << __LINE__ << endl;
 			exit(EXIT_FAILURE);
 		}
-    //else { for(auto e : numbers_bob) cout << e << ' '; cout << endl; }
 
+		// Extract to lambda blob parameter
 		vector<char> numbers_jack;
-		db << "SELECT numbers from person where name = ?;" << "jack" >> numbers_jack;
+		db << "SELECT numbers from person where name = ?;" << "jack" >> [](blob_t<vector<char>> numbers_jack) {
+			if(numbers_jack.vec.size() != 3 || numbers_jack.vec[0] != '1'
+	        || numbers_jack.vec[1] != '2' || numbers_jack.vec[2] != '3') {
+				cout << "Bad result on line " << __LINE__ << endl;
+				exit(EXIT_FAILURE);
+			}
+		};
 
-		if(numbers_jack.size() != 4 || numbers_jack[0] != '1'
-        || numbers_jack[1] != '2' || numbers_jack[2] != '3' || numbers_jack[3] != '4' ) {
+		// Extract to rvalue blob 
+		auto numbers_sara = db << "SELECT numbers from person where name = ?;" << "sara" >> blob(vector<double>());
+
+		if(numbers_sara.size() != 3 || numbers_sara[0] != 1 || numbers_sara[1] != 2 || numbers_sara[2] != 3) {
 			cout << "Bad result on line " << __LINE__ << endl;
 			exit(EXIT_FAILURE);
 		}
-    //else { for(auto e : numbers_jack) cout << e << ' '; cout << endl; }
-
-		vector<double> numbers_sara;
-		db << "SELECT numbers from person where name = ?;" << "sara" >> numbers_sara;
-
-		if(numbers_sara.size() != 4 || numbers_sara[0] != 1
-        || numbers_sara[1] != 2 || numbers_sara[2] != 3 || numbers_sara[3] != 4 ) {
-			cout << "Bad result on line " << __LINE__ << endl;
-			exit(EXIT_FAILURE);
-		}
-		//else {
-			//db << "SELECT numbers from person where name = ?;" << "sara" >> [](vector<double> numbers_sara){
-			    //for(auto e : numbers_sara) cout << e << ' '; cout << endl;
-			//};
-		//}
-
 	}
 	catch(sqlite_exception e)
 	{
